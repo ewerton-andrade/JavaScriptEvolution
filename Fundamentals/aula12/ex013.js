912012b073c5de7a2e48cbbecf1cf05ed703ad2b
ewerton-andrade <etonandrade@gmail.com>
2022-07-11 08:53:51 -0400

    Update ex013.js

--- Fundamentals/aula12/ex013.js
+++ Fundamentals/aula12/ex013.js
@@ -3,10 +3,29 @@ diaSemana = agora.getHours()
 
 switch(diaSemana) {
     case 0:
-        console.log('Domingo')
+        console.log('Sunday')
         break
 
     case 1:
-        console.log('Segunda')
+        console.log('Monday')
         break
+
+    case 2:
+        console.log('Tuesday')
+        break
+
+    case 3:
+        console.log('Wednesday')
+        break
+
+    case 4:
+        console.log('Thursday')
+        break
+
+    case 5:
+        console.log('Friday')
+        break
+
+    case 6:
+        console.log('Saturday')
 }
