ba50e52fe6e1058dc2fffd37734442f4eacfcb19
ewerton-andrade <etonandrade@gmail.com>
2022-07-15 17:45:58 -0400

    DropList Component Created

--- react-projects/organo/src/App.js
+++ react-projects/organo/src/App.js
@@ -1,13 +1,12 @@
 import Banner from './componentes/Banner';
-import TextField from './componentes/TextField';
+import Form from './componentes/Form';
+
 
 function App() {
   return (
     <div className="App">
       <Banner/>
-      <TextField label="Nome" placeholder="Digite o seu nome"/>
-      <TextField label="Cargo" placeholder="Digite o seu cargo"/>
-      <TextField label="Imagem" placeholder="Digite o endereco da imagem"/>
+      <Form/>
     </div>
   );
 }
---
+++ react-projects/organo/src/componentes/Form/index.js
@@ -0,0 +1,30 @@
+import "./Form.css";
+import TextField from "../TextField";
+import DropList from "../DropList";
+
+const Form = () => {
+
+const teams = [
+    'Coding', 
+    'Front-End', 
+    'Data Science', 
+    'Devops', 
+    'UX e Design', 
+    'Mobile', 
+    'Management and Inovation'
+]
+
+    return (
+        <section className="Form">
+            <form>
+                <h2>Fill the data to create a worker card</h2>
+                <TextField label="Name" placeholder="Insert your name" />
+                <TextField label="Charge" placeholder="Insert your charge" />
+                <TextField label="Image" placeholder="Insert your image url" />
+                <DropList label='Team' itens={teams}/>
+            </form>
+        </section>
+    )
+}
+
+export default Form
